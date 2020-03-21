59b82abb46fae2c22ea4300db63b93a90697511c
kuro-123 <noripiro@sage.ocn.ne.jp>
2020-03-21 21:57:01 +0900

    Nukkitコマンド無効化設定機能追加

--- src/main/java/host/kuro/onetwothree/command/CommandManager.java
+++ src/main/java/host/kuro/onetwothree/command/CommandManager.java
@@ -1,17 +1,35 @@
 package host.kuro.onetwothree.command;
 
+import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandMap;
 import host.kuro.onetwothree.OneTwoThreeAPI;
 import host.kuro.onetwothree.command.defaults.NameCommand;
 import host.kuro.onetwothree.command.defaults.WarpCommand;
 import host.kuro.onetwothree.command.defaults.XboxCommand;
 
+import java.util.Map;
+
 public class CommandManager {
 
     public static void registerAll(OneTwoThreeAPI api) {
-    	CommandMap map = api.getServer().getCommandMap();
-        map.register("OneTwoThreeBase", new WarpCommand(api));
-        map.register("OneTwoThreeBase", new NameCommand(api));
-        map.register("OneTwoThreeBase", new XboxCommand(api));
+        try {
+            // 無効か
+            String disableCommands = api.getConfig().getString("Command.Disable");
+            if (disableCommands.length() > 0) {
+                String[] cmds = disableCommands.split(",", 0);
+                if (cmds.length > 0) {
+                    for (String target : cmds) {
+                        api.getServer().getCommandMap().getCommands().remove(target);
+                    }
+                }
+            }
+            // 登録
+            CommandMap map = api.getServer().getCommandMap();
+            map.register("OneTwoThreeBase", new WarpCommand(api));
+            map.register("OneTwoThreeBase", new NameCommand(api));
+            map.register("OneTwoThreeBase", new XboxCommand(api));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
